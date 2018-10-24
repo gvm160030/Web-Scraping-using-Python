1aeb2298d6ae5c82c7b3511e38d75cbe93587b32
Gauri Mutha <gvm160030@utdallas.edu>
2018-10-24 01:12:08 -0500

    Python code and reference document with related step information

---
+++ Example2_XXX.py
@@ -0,0 +1,26 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf')
+
+import csv
+import requests
+from bs4 import BeautifulSoup
+url = 'http://espn.go.com/college-football/bcs/_/year/2013'
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup((html),"lxml")
+table = soup.find('table', attrs={'class':'tablehead'})
+
+list_of_rows = []
+for row in table.findAll('tr')[0:]:
+	list_of_cells = []
+	for cell in row.findAll('td'):
+		text = cell.text.replace('&nbsp;','')
+		list_of_cells.append(text)
+	list_of_rows.append(list_of_cells)
+
+outfile = open("./Rankings.csv","wb")
+writer = csv.writer(outfile)
+writer.writerows(list_of_rows)
+ 
